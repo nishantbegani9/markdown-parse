9126887127885c0bc6553f3abe4587834844a630
nishantbegani9 <91592178+nishantbegani9@users.noreply.github.com>
2022-01-19 16:47:01 -0800

    changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,14 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
         }
         return toReturn;
     }
---
+++ test-file2.md
@@ -0,0 +1,6 @@
+# Title
+
+here is an open bracked: [
+here is an closed parenthesis: )
+
+[a link](http:// google.com/this()/here)
